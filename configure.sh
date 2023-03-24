4b113a8acf2f6604ae3a6cbbe6b32438087d00f1
Matthieu Archambault <44174690+archamm@users.noreply.github.com>
2023-03-24 11:48:03 +0100

    Update configure.sh vm public IP

--- configure.sh
+++ configure.sh
@@ -22,7 +22,7 @@ sudo bash -c "cat > /etc/httpd/conf.d/react-app.conf << EOL
     # Set the document root to the build output directory of your React app
     DocumentRoot /home/ec2-user/centrale-nantes-ec2-app/build
     # Set the server name to the public DNS of your EC2 instance
-    ServerName ec2-13-38-89-246.eu-west-3.compute.amazonaws.com
+    ServerName ec2-13-38-250-126.eu-west-3.compute.amazonaws.com
 
     # Configure the directory containing your React app
     <Directory /home/ec2-user/centrale-nantes-ec2-app/build>
